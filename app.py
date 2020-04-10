32ad617e1fd31dfea15ab5a39b965adb5494a68c
js.yoo <js.yoo@ttb.co.kr>
2020-04-10 21:38:03 +0900

    slackbot.py update

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
+from slacker import Slacker
 
 from common import slack_signing_secret, slack_bot_token
+from common.logger import Logger
+from slackbot.slackbot import SlackBot
 
 
 # This `app` represents your existing Flask app
@@ -19,12 +22,24 @@ def hello():
 slack_event_adapter = SlackEventAdapter(slack_signing_secret, '/slack/events', app)
 
 
+# Slacker setting
+slack_client = Slacker(slack_bot_token)
+
+
 # Create an event listener for "message" events and print the message
 @slack_event_adapter.on('message')
 def handle_message(event_data):
     message = event_data['event']
+
+    # If the incoming message respond
+    if message.get('subtype') is None:
+        msg = message.get('text')
+        channel = message['channel']
+        response = slackbat.handle_command(msg)
+        slack_client.chat.post_message(channel, msg)
+
     print(message)
-    
+
 
 # Create an event listener for "reaction_added" events and print the emoji name
 @slack_event_adapter.on('reaction_added')
@@ -35,4 +50,5 @@ def reaction_added(event_data):
 
 # Start the server on port 3000
 if __name__ == '__main__':
+    slackbat = SlackBot()
     app.run(host='0.0.0.0', port=3000)
--- slackbot/slackbot.py
+++ slackbot/slackbot.py
@@ -1,5 +1,23 @@
-from slackeventsapi import SlackEventAdapter
 from common.logger import Logger
-from common import slack_bot_token, slack_signing_secret
 
 
+class SlackBot(object):
+    logger = Logger('SlackBot')
+
+    def __init__(self):
+        return None
+    
+    def  handle_command(self, command):
+        response = None
+        try:
+            if command == 'help' or command == '도움말':
+                response = "도움말"
+        except Exception as e:
+            self.logger.debug(e)
+            response = "오류"
+
+        return response
+    
+    def start(self):
+        self.logger.debug('Starter Bot connected and running!')
+        return None
