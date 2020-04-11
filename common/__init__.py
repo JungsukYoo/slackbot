e58a1b326dfca5862ba3f693e5e73f48bea95f1e
js.yoo <js.yoo@ttb.co.kr>
2020-04-12 03:31:05 +0900

    SLACK_SIGNING_SECRET 변경

--- common/__init__.py
+++ common/__init__.py
@@ -5,6 +5,6 @@ import json
 with open('./config.json') as json_file:
     config = json.load(json_file)
     log_level = config['log_level']
-    slack_signing_secret = config['slack_signing_secret']
 
+slack_signing_secret = os.environ['SLACK_SIGNING_SECRET']
 slack_bot_token = os.environ['SLACK_BOT_TOKEN']
